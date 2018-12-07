bbd0b85987cbb89a8186febe744e8a0406482a57
gmy <gemingyu@126.com>
2018-12-07 19:43:24 +0800

    fix bug right ads to be solved

--- rm-rf-csdnads.js
+++ rm-rf-csdnads.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove CSDN ADs
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.5
 // @description  try to take over the world!
 // @author       Admingyu
 // @match        https://blog.csdn.net/*
@@ -10,7 +10,10 @@
 // ==/UserScript==
 
 //左栏广告
-$("#asideFooter").remove();
+
+$(".csdn-tracking-statistics.mb8.box-shadow").remove();
+
+
 
 //左侧浮动广告
 $(".fourth_column").remove();
@@ -21,13 +24,31 @@ $(".pulllog-box").remove();
 //右侧广告
 $("#pic_container").remove();
 $(".pic_container").remove();
-$("#iframeu*").remove();
+$("#img-txt-wrapper").remove();
+
+//<a href="https://h5.youzan.com/v2/goods/3f5drkf7by4en" target="_blank"><div class="context-box h5width">
+$("a[href^='https://h5.youzan.com/v']").parent().remove()
+
+//<div id="kp_box_65" data-pid="65" data-track-click="{&quot;mod&quot;:&quot;kp_popu_65-625&quot;,&quot;con&quot;:&quot;,,&quot;}"><div style="width: 100%;"><
+$("div[id^='kp_box_']").parent().remove()
+
+$("iframe[id^='iframeu']").parent().remove()
+
+
+
+
 
 //腹部广告
-$("#recommend-item-box recommend-ad-box").remove();
+$(".recommend-item-box.recommend-ad-box").remove();
+$("#kp_box_*").remove();
+$(":contains('.youzan.com/')").remove();
+$(".mediav_ad").remove();//effecient
 //$("#iframeu*").remove();
-//$("#iframeu*").remove();
-//$("#iframeu*").remove();
+
+
+//其他广告
+$(":contains('百度广告')").remove()
+
 
 
 
