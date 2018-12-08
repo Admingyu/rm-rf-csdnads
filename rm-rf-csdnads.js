fabc8d7bcd5b3707986d86b3b622b3056711f3de
gmy <gemingyu@126.com>
2018-12-08 17:37:27 +0800

    完善右侧广告屏蔽功能

--- rm-rf-csdnads.js
+++ rm-rf-csdnads.js
@@ -22,19 +22,19 @@ $(".fourth_column").remove();
 $(".pulllog-box").remove();
 
 //右侧广告
-$("#pic_container").remove();
-$(".pic_container").remove();
-$("#img-txt-wrapper").remove();
+//$("#pic_container").remove();
+//$(".pic_container").remove();
+//$("#img-txt-wrapper").remove();
 
 //<a href="https://h5.youzan.com/v2/goods/3f5drkf7by4en" target="_blank"><div class="context-box h5width">
-$("a[href^='https://h5.youzan.com/v']").parent().remove()
+//$("a[href^='https://h5.youzan.com/v']").parent().remove()
 
 //<div id="kp_box_65" data-pid="65" data-track-click="{&quot;mod&quot;:&quot;kp_popu_65-625&quot;,&quot;con&quot;:&quot;,,&quot;}"><div style="width: 100%;"><
-$("div[id^='kp_box_']").parent().remove()
+//$("div[id^='kp_box_']").parent().remove()
 
-$("iframe[id^='iframeu']").parent().remove()
-
-
+$(document).ready(function(){
+  $("iframe[id^='iframeu']").remove()
+});
 
 
 
